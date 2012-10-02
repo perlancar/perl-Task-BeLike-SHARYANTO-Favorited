b1154ccb4331e65926fc2f07bfaa1f5187bb94da
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-10-02 10:44:55 +0700

    Initial commit, v0.01

---
+++ lib/Task/BeLike/SHARYANTO/Favorited.pm
@@ -0,0 +1,148 @@
+package Task::BeLike::SHARYANTO::Favorited;
+
+# VERSION
+
+1;
+# ABSTRACT: Install all SHARYANTO's favorite modules
+
+=head1 DESCRIPTION
+
+This task will install modules favorited by SHARYANTO on L<http://metacpan.org>.
+
+=pkgroup Included modules
+
+=pkg Data::Rmap
+
+=pkg HTML::Escape
+
+=pkg App::PPI::Dumper
+
+=pkg Scalar::Util::Numeric
+
+=pkg perlsecret
+
+=pkg App::cpanoutdated
+
+=pkg cpan::listchanges
+
+=pkg App::pmuninstall
+
+=pkg App::perlbrew
+
+=pkg Config::IniFiles
+
+=pkg Regexp::Grammars
+
+=pkg Plack
+
+=pkg PSGI
+
+=pkg Starman
+
+=pkg Class::Inspector
+
+=pkg Text::ASCIITable
+
+=pkg Term::ANSIColor
+
+=pkg syntax
+
+=pkg File::Flock
+
+=pkg Module::Path
+
+=pkg Module::List
+
+=pkg Sort::Versions
+
+=pkg Digest::MD5
+
+=pkg Dist::Zilla
+
+=pkg Test::Exception
+
+=pkg Test::Simple
+
+=pkg REST::Google
+
+=pkg CHI
+
+=pkg Tie::Cache
+
+=pkg WWW::Mechanize
+
+=pkg String::ShellQuote
+
+=pkg Proc::PID::File
+
+=pkg Path::Class
+
+=pkg Mouse
+
+=pkg Moose
+
+=pkg Mojolicious
+
+=pkg YAML::LibYAML
+
+=pkg YAML::Syck
+
+=pkg YAML
+
+=pkg List::MoreUtils
+
+=pkg JSON
+
+=pkg LWP
+
+=pkg File::Which
+
+=pkg File::HomeDir
+
+=pkg DBD::SQLite
+
+=pkg DBI
+
+=pkg autodie
+
+=pkg DateTime
+
+=pkg DateTime::BusinessHours
+
+=pkg Moo
+
+=pkg Data::Clone
+
+=pkg UUID::Random
+
+=pkg Capture::Tiny
+
+=pkg File::LibMagic
+
+=pkg Devel::Platform::Info
+
+=pkg File::Slurp
+
+=pkg File::chdir
+
+=pkg App::cpanminus
+
+=pkg Data::Printer
+
+=pkg Carp::Always::Color
+
+=pkg Carp::Always
+
+=pkg Taint::Util
+
+=pkg App::perlfind
+
+=pkg PerlX::Maybe
+
+=pkg Data::DPath
+
+=pkg Log::Any
+
+=pkg Marpa
+
+=cut
